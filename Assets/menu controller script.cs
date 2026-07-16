c9f43cd836e60a4a9db6a295e6b0d2889f9e115b
Ammaar Saeed <ammaars2007@gmail.com>
2026-07-16 12:02:38 +0530

    added buttons
    added very simple UI buttons and an almost empty script for now.

---
+++ Assets/menu controller script.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+
+public class menucontrollerscript
+{
+    GameObject startButton;
+    void Start()
+    {
+        
+    }
+    public void OnStartButtonClicked()
+    {
+        Debug.Log(startButton.name);
+    }
+}
